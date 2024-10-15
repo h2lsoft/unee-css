091c0c90f4e8ae1a00abef58803b4aef3b690748
H2LSOFT <contact@h2lsoft.com>
2024-10-15 16:25:31 +0200

    minor bug fixed

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	plugins: [
+		require('postcss-sort-media-queries')({
+			sort: 'desktop-first',
+		}),
+		require('autoprefixer'),
+		require('cssnano')({
+			preset: 'default',
+		}),
+	]
+}
